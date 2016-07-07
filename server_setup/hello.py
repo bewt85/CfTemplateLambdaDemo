6c22f4818dd4d49dbfed2432c12ae84e0ab168f5
Ben Taylor <ben@drie.co>
2016-07-07 12:05:38 +0100

    Better hello app

--- server_setup/hello.py
+++ server_setup/hello.py
@@ -1,9 +1,26 @@
 from flask import Flask
 app = Flask(__name__)
 
+import datetime
 import os
-import random
 
 @app.route('/')
 def hello_world():
-    return 'Hello, {} from {}!'.format(random.choice(["foo", "bar", "baz"]), os.getenv("APP_NAME", "Anon"))
+    template = """\
+<h1>Hello, World!</h1>
+<p>The time is {time}</p>
+<p>Here is my config</p>
+<ul>
+  <li>APP_NAME={app_name}</li>
+  <li>APP_VERSION={app_version}</li>
+  <li>APP_SCALE={app_scale}</li>
+</ul>
+"""
+    parameters = {
+        "time": str(datetime.datetime.now()),
+        "app_name": os.getenv("APP_NAME", "Unknown"),
+        "app_version": os.getenv("APP_VERSION", "Unknown"),
+        "app_scale": os.getenv("APP_SCALE", "Unknown")
+    }
+
+    return template.format(**parameters)
